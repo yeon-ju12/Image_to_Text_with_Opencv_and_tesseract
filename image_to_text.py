09fe30e3ef3a68486f584eee5e4c7f7053231423
Lee eunwoo <ee041225@naver.com>
2024-12-05 02:06:04 +0900

    modify code

--- image_to_text.py
+++ image_to_text.py
@@ -20,7 +20,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
-
 import cv2
 import numpy as np
 import pytesseract
@@ -36,11 +35,10 @@ if not os.path.exists('crops'):
 
 # 이미지 로드
 image_file = sys.argv[1]
-large = cv2.imread(image_file)
-if large is None:
+rgb = cv2.imread(image_file)
+if rgb is None:
     print("NO image")
     sys.exit(1)
-rgb = cv2.pyrDown(large)  # 이미지 크기 축소
 small = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
 
 # 모폴로지 그라디언트
@@ -68,15 +66,36 @@ for idx in range(len(contours)):
     cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
     r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
 
+    padding = 8
     # 텍스트 영역으로 판단될 조건
     if r > 0.45 and w > 8 and h > 8:
+        # 여백 추가된 크롭 영역 계산
+        # 여백 추가된 크롭 영역 계산
+        x_pad = max(0, x - padding)  # 왼쪽 경계
+        y_pad = max(0, y - padding)  # 위쪽 경계
+        x2_pad = min(rgb.shape[1], x + w + padding)  # 오른쪽 경계
+        y2_pad = min(rgb.shape[0], y + h + padding)  # 아래쪽 경계
+
+# 여백을 추가한 크롭된 이미지 저장
+        cropped = rgb[y_pad:y2_pad, x_pad:x2_pad]
+
+
         # 크롭된 이미지 저장 (crops 폴더에 저장)
-        cropped = rgb[y:y+h, x:x+w]
+        crop_gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+        
+        
+        _, crop_bin = cv2.threshold(crop_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+
+        resized = cv2.resize(crop_bin, None, fx=2.5, fy=2.5, interpolation=cv2.INTER_CUBIC)
+
+
         cropped_filename = f"crops/cropped_{index}.jpg"
-        cv2.imwrite(cropped_filename, cropped)
+        cv2.imwrite(cropped_filename, resized)
         
         # 텍스트 인식
-        text = pytesseract.image_to_string(cropped, lang='eng+kor')
+        custom_config = r'--oem 3 --psm 8'
+        text = pytesseract.image_to_string(resized, lang='eng+kor', config=custom_config)
         
         # 공백인 텍스트는 출력하지 않음
         if text.strip():
